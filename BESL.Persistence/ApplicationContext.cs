06c8792ccd365aa9ae95adbe1b2ae3c1c30c0610
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-08 19:28:57 +0300

    Extracted database domains and context to seperate assemblies, added DesignTimeDbContext factory and created initial migration.

---
+++ BESL.Persistence/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using BESL.Domain.Entities;
+
+namespace BESL.Persistence
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ BESL.Persistence/ApplicationContextFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace BESL.Persistence
+{
+    using BESL.Persistence.Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationContextFactory : DesignTimeDbContextFactoryBase<ApplicationContext>
+    {
+        protected override ApplicationContext CreateNewInstance(DbContextOptions<ApplicationContext> options)
+        {
+            return new ApplicationContext(options);
+        }
+    }
+}
