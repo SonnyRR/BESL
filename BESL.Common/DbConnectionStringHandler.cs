8f3788c57bda45cd04965875cc46f3e37d4ee441
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-08 20:42:04 +0300

    Added documentation to DbConnectionStringHandler.cs

--- BESL.Common/DbConnectionStringHandler.cs
+++ BESL.Common/DbConnectionStringHandler.cs
@@ -2,8 +2,17 @@
 {   
     using System.Runtime.InteropServices;
 
+    /// <summary>
+    /// Class that manages connection string key names depending on the current OS.
+    /// </summary>
     public class DbConnectionStringHandler
     {
+        /// <summary>
+        /// Runtime checks the current OS and returns a <see cref="System.String"/>
+        /// with the key name of the connection string.
+        /// </summary>
+        /// <remarks>Values must match these in appsettings.json as they are hardcoded!</remarks>
+        /// <returns>A <see cref="System.String"/> with the key value of the connection string.</returns>
         public static string GetConnectionStringNameForCurrentOS()
         {
             string connectionStringName = "DefaultConnection";
