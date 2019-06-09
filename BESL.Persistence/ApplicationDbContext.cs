ce943d94bf6b99549854c1c98553eb1137d12643
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-09 23:39:58 +0300

    Removed unecessary files.

--- BESL.Persistence/ApplicationDbContext.cs
+++ BESL.Persistence/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
 ﻿namespace BESL.Persistence
 {
-    using System;
     using System.Linq;
-    using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -10,16 +8,10 @@
     using Microsoft.EntityFrameworkCore;
 
     using BESL.Domain.Entities;
-    using BESL.Domain.Infrastructure;
     using BESL.Persistence.Infrastructure;
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        private static readonly MethodInfo SetIsDeletedQueryFilterMethod =
-            typeof(ApplicationDbContext).GetMethod(
-                nameof(SetIsDeletedQueryFilter),
-                BindingFlags.NonPublic | BindingFlags.Static);
-
+    {       
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
@@ -27,22 +19,28 @@
 
         public DbSet<Setting> Settings { get; set; }
 
+        public DbSet<Player> Players { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Match> Matches { get; set; }
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
         {
-            this.ApplyAuditInfoRules();
             return base.SaveChanges(acceptAllChangesOnSuccess);
         }
 
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) =>
-            this.SaveChangesAsync(true, cancellationToken);
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) 
+            => this.SaveChangesAsync(true, cancellationToken);
 
         public override Task<int> SaveChangesAsync(
             bool acceptAllChangesOnSuccess,
             CancellationToken cancellationToken = default)
         {
-            this.ApplyAuditInfoRules();
             return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
 
@@ -51,20 +49,9 @@
             // Needed for Identity models configuration
             base.OnModelCreating(builder);
 
-            ConfigureUserIdentityRelations(builder);
-
-            EntityIndexesConfiguration.Configure(builder);
+            ConfigureUserIdentityRelations(builder);            
 
             var entityTypes = builder.Model.GetEntityTypes().ToList();
-
-            // Set global query filter for not deleted entities only
-            var deletableEntityTypes = entityTypes
-                .Where(et => et.ClrType != null && typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
-            foreach (var deletableEntityType in deletableEntityTypes)
-            {
-                var method = SetIsDeletedQueryFilterMethod.MakeGenericMethod(deletableEntityType.ClrType);
-                method.Invoke(null, new object[] { builder });
-            }
 
             // Disable cascade delete
             var foreignKeys = entityTypes
@@ -97,35 +84,6 @@
                 .HasForeignKey(e => e.UserId)
                 .IsRequired()
                 .OnDelete(DeleteBehavior.Restrict);
-        }
-
-        private static void SetIsDeletedQueryFilter<T>(ModelBuilder builder)
-            where T : class, IDeletableEntity
-        {
-            builder.Entity<T>().HasQueryFilter(e => !e.IsDeleted);
-        }
-
-        private void ApplyAuditInfoRules()
-        {
-            var changedEntries = this.ChangeTracker
-                .Entries()
-                .Where(e =>
-                    e.Entity is IAuditInfo &&
-                    (e.State == EntityState.Added || e.State == EntityState.Modified));
-
-            foreach (var entry in changedEntries)
-            {
-                var entity = (IAuditInfo)entry.Entity;
-
-                if (entry.State == EntityState.Added && entity.CreatedOn == default)
-                {
-                    entity.CreatedOn = DateTime.UtcNow;
-                }
-                else
-                {
-                    entity.ModifiedOn = DateTime.UtcNow;
-                }
-            }
         }
     }
 }
