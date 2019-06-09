795b70a1fcee1a649815ec7791a7ae237782a4aa
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-09 16:56:29 +0300

    Fixed removed files from commit revert.

--- BESL.Web/Startup.cs
+++ BESL.Web/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using BESL.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using BESL.Persistence;
