66856d7cb3cc22edb3eb2acad0efc23a922adb59
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-09 16:03:13 +0300

    Added sandbox console app.

---
+++ BESL.SandBox/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BESL.SandBox
+{
+    using BESL.Persistence;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            ApplicationContextFactory factory = new ApplicationContextFactory();
+            DbContextOptions<ApplicationContext> options = new DbContextOptions<ApplicationContext>();
+            factory.CreateDbContext(null);
+
+        }
+    }
+}
