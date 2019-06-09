fabea61b7b86b00d36163bd4b3ab28d81204babe
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-09 14:39:33 +0300

    Updated Layouts and refactored User.cs

--- BESL.Domain/Entities/User.cs
+++ BESL.Domain/Entities/User.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-
-namespace BESL.Domain.Entities
+﻿namespace BESL.Domain.Entities
 {
-    // Add profile data for application users by adding properties to the User class
+    using Microsoft.AspNetCore.Identity;
+    
     public class User : IdentityUser
     {
+
     }
 }
