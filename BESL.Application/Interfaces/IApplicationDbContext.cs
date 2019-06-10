0b0e945451a6f6bc76302609db99dfe37eeecfba
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-10 13:16:16 +0300

    Added IApplicationDbContext interface.

---
+++ BESL.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace BESL.Application.Interfaces
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.EntityFrameworkCore;
+
+    using BESL.Domain.Entities;
+
+    public interface IApplicationDbContext
+    {
+        DbSet<Setting> Settings { get; set; }
+
+        DbSet<Player> Players { get; set; }
+
+        DbSet<Game> Games { get; set; }
+
+        DbSet<Team> Teams { get; set; }
+
+        DbSet<Match> Matches { get; set; }
+
+        DbSet<PlayerTeam> PlayerTeams { get; set; }
+
+        DbSet<PlayerMatch> PlayerMatch { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
