7f8daa065437d05662e37f98bea8ef8512e6b5bb
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-10 12:16:38 +0300

    Added BaseController class abstraction and changed project references, introduced AutoMapper and MediatR.

---
+++ BESL.Web/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿namespace BESL.Web.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class BaseController : Controller
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
--- BESL.Web/Startup.cs
+++ BESL.Web/Startup.cs
@@ -1,23 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using BESL.Persistence;
-using BESL.Domain.Entities;
-using BESL.Common;
-
+﻿
 namespace BESL.Web
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.UI;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MediatR;
+
+    using BESL.Persistence;
+    using BESL.Domain.Entities;
+    using BESL.Common;
+    using BESL.Application;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -46,6 +51,8 @@ namespace BESL.Web
                     .AddEntityFrameworkStores<Persistence.ApplicationDbContext>();
 
                 services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+                services.AddMediatR(typeof(TestClass).Assembly);                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
