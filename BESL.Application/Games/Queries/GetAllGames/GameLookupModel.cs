27ca74437839f51c2251de50253ada2af57ca8f6
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-21 14:33:44 +0300

    Added Games/Query and updated solution.

---
+++ BESL.Application/Games/Queries/GetAllGames/GameLookupModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BESL.Application.Games.Queries.GetAllGames
+{
+    public class GameLookupModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int CurrentActiveTournaments { get; set; }
+
+        public int RegisteredTeams { get; set; }
+    }
+}
