fb1a772d1be25c3d20bb36645828a1fc23e70ae5
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-21 14:43:58 +0300

    Updated GameLookupModel.

--- BESL.Application/Games/Queries/GetAllGames/GameLookupModel.cs
+++ BESL.Application/Games/Queries/GetAllGames/GameLookupModel.cs
@@ -1,5 +1,10 @@
 ﻿namespace BESL.Application.Games.Queries.GetAllGames
 {
+    using System.Linq;
+
+    using AutoMapper;
+    using BESL.Domain.Entities;
+
     public class GameLookupModel
     {
         public string Name { get; set; }
@@ -9,5 +14,12 @@
         public int CurrentActiveTournaments { get; set; }
 
         public int RegisteredTeams { get; set; }
+
+        public void CreateMappings(Profile configuration)
+        {
+            configuration.CreateMap<Game, GameLookupModel>()
+                .ForMember(glm => glm.CurrentActiveTournaments, o => o.MapFrom(g => g.Competitions.Count(c=>c.IsActive)))
+                .ForMember(glm => glm.RegisteredTeams, o => o.MapFrom(g => g.Competitions.Count));
+        }
     }
 }
