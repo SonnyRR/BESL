42f189723ab89891017ff4ee8fa04c2c95e0fafb
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-22 19:21:27 +0300

    Addec CreateTournament command and validator classes.

---
+++ BESL.Application/Tournaments/Commands/CreateTournamentCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace BESL.Application.Tournaments.Commands
+{
+    using MediatR;
+
+    public class CreateTournamentCommand : IRequest
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int FormatId { get; set; }
+    }
+}
---
+++ BESL.Application/Tournaments/Commands/CreateTournamentCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿namespace BESL.Application.Tournaments.Commands
+{
+    using FluentValidation;
+
+    public class CreateTournamentCommandValidator : AbstractValidator<CreateTournamentCommand>
+    {
+        private const int NAME_MIN_LENGTH = 3, NAME_MAX_LENGTH = 35;
+        private const int DESC_MIN_LENGTH = 0, DESC_MAX_LENGTH = 200;
+
+        private const string NAME_LENGTH_MSG = "Tournament name length must be between {0} and {1} characters long!";
+        private const string DESC_LENGTH_MSG = "Tournament description length must be between {0} and {1} characters long!";
+
+
+        public CreateTournamentCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .Length(NAME_MIN_LENGTH, NAME_MAX_LENGTH)
+                .WithMessage(string.Format(NAME_LENGTH_MSG, NAME_MIN_LENGTH, NAME_MAX_LENGTH));
+
+            RuleFor(x => x.Description)
+                .Length(DESC_MIN_LENGTH, DESC_MAX_LENGTH)
+                .WithMessage(string.Format(DESC_LENGTH_MSG, DESC_MIN_LENGTH, DESC_MAX_LENGTH));
+        }
+    }
+}
