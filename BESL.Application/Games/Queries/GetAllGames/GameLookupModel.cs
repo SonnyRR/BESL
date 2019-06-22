943b1b605afb80329c01b7d57cfa0bdcab0761e8
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-22 09:29:22 +0300

    Added ID property to GameLookupModel.

--- BESL.Application/Games/Queries/GetAllGames/GameLookupModel.cs
+++ BESL.Application/Games/Queries/GetAllGames/GameLookupModel.cs
@@ -9,6 +9,8 @@
 
     public class GameLookupModel : IHaveCustomMapping
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
