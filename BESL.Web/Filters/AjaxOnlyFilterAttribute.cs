fbed606a574056a5179dc6d289faf6c99af5e940
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-24 00:32:58 +0300

    Added AjaxOnlyFilter.

---
+++ BESL.Web/Filters/AjaxOnlyFilterAttribute.cs
@@ -0,0 +1,44 @@
+﻿namespace BESL.Web.Filters
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class AjaxOnlyFilterAttribute : ActionFilterAttribute, IAsyncActionFilter
+    {
+        private const string RequestedWithHeader = "X-Requested-With";
+        private const string XmlHttpRequest = "XMLHttpRequest";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var request = context.HttpContext.Request;
+
+            if (this.IsAjaxRequest(request))
+            {
+                await next();
+            }
+            else
+            {
+                context.Result = new ForbidResult();
+            }
+        }
+
+        private bool IsAjaxRequest(HttpRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            if (request.Headers != null)
+            {
+                return request.Headers[RequestedWithHeader] == XmlHttpRequest;
+            }
+
+            return false;
+        }
+    }
+}
