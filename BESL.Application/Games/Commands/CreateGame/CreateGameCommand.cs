a1bef59679477e167bfc9afeefede3e3517fbc51
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-24 21:56:26 +0300

    Updated Games command and Handler.

--- BESL.Application/Games/Commands/CreateGame/CreateGameCommand.cs
+++ BESL.Application/Games/Commands/CreateGame/CreateGameCommand.cs
@@ -1,11 +1,14 @@
 ﻿namespace BESL.Application.Games.Commands.CreateGame
 {
     using MediatR;
+    using Microsoft.AspNetCore.Http;
 
     public class CreateGameCommand : IRequest<int>
     {
         public string Name { get; set; }
 
         public string Description { get; set; }
+
+        public IFormFile GameImage { get; set; }
     }
 }
--- BESL.Application/Games/Commands/CreateGame/CreateGameCommandHandler.cs
+++ BESL.Application/Games/Commands/CreateGame/CreateGameCommandHandler.cs
@@ -4,21 +4,31 @@
     using System.Threading.Tasks;
 
     using MediatR;
+    using Microsoft.Extensions.Configuration;
 
     using BESL.Application.Interfaces;
+    using BESL.Common;
     using BESL.Domain.Entities;
+    using CloudinaryDotNet.Actions;
+    using CloudinaryDotNet;
 
     public class CreateGameCommandHandler : IRequestHandler<CreateGameCommand, int>
     {
         private readonly IApplicationDbContext context;
+        private readonly IConfiguration configuration;
 
-        public CreateGameCommandHandler(IApplicationDbContext context)
+        public CreateGameCommandHandler(IApplicationDbContext context, IConfiguration configuration)
         {
             this.context = context;
+            this.configuration = configuration;
         }
 
         public async Task<int> Handle(CreateGameCommand request, CancellationToken cancellationToken)
         {
+            var cloudinary = CloudinaryHelper.GetInstance(this.configuration);
+
+            var url = await CloudinaryHelper.UploadImage(cloudinary, request.GameImage, $"{request.Name}-main-shot");
+
             Game game = new Game()
             {
                 Name = request.Name,
