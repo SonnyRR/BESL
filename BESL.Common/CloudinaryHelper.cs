32b91210ac92fc9582a8a34dd54f18fb491c643d
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-24 21:56:04 +0300

    Moved CloudinaryHelper to BESL.Common and updated assembly target framework to netstandard2.0.

--- BESL.Common/CloudinaryHelper.cs
+++ BESL.Common/CloudinaryHelper.cs
@@ -1,6 +1,10 @@
-﻿namespace BESL.Web.Helpers
+﻿namespace BESL.Common
 {
+    using System.IO;
+    using System.Threading.Tasks;
     using CloudinaryDotNet;
+    using CloudinaryDotNet.Actions;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Configuration;
 
     public static class CloudinaryHelper
@@ -20,6 +24,34 @@
 
             Cloudinary instance = new Cloudinary(account);
             return instance;
+        }
+
+        public static async Task<string> UploadImage(Cloudinary cloudinary, IFormFile fileForm, string name)
+        {
+            if (fileForm == null)
+            {
+                return null;
+            }
+
+            byte[] image;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await fileForm.CopyToAsync(memoryStream);
+                image = memoryStream.ToArray();
+            }
+
+            var ms = new MemoryStream(image);
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(name, ms),             
+            };
+
+            var uploadResult = cloudinary.Upload(uploadParams);
+
+            ms.Dispose();
+            return uploadResult.SecureUri.AbsoluteUri;
         }
     }
 }
