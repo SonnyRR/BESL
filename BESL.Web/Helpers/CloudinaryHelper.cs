c092eeef348e93acd8b7220a21c72a446f4c04b5
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-24 18:47:54 +0300

    Introduced CloudinaryHelper.

---
+++ BESL.Web/Helpers/CloudinaryHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace BESL.Web.Helpers
+{
+    using CloudinaryDotNet;
+    using Microsoft.Extensions.Configuration;
+
+    public static class CloudinaryHelper
+    {
+        public static Cloudinary GetInstance(IConfiguration configuration)
+        {
+            /*
+             * dotnet user-secrets set "cloudinary-cloud" "your_cloud_id"
+             * dotnet user-secrets set "cloudinary-apiKey" "your_api_key"
+             * dotnet user-secrets set "cloudinary-apiSecret" "your_api_secret"
+             */
+            var cloud = configuration["cloudinary-cloud"];
+            var apiKey = configuration["cloudinary-apiKey"];
+            var apiSecret = configuration["cloudinary-apiSecret"];
+
+            Account account = new Account(cloud, apiKey, apiSecret);
+
+            Cloudinary instance = new Cloudinary(account);
+            return instance;
+        }
+    }
+}
