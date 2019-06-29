9f8c0a2ec7f33d63450051d19507cd86f24731c3
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-29 18:34:01 +0300

    Added CreateTournamentFormatCommand.

---
+++ BESL.Application/Formats/Commands/CreateTournamentFormatCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace BESL.Application.Formats.Commands
+{
+    public class CreateTournamentFormatCommand
+    {
+        public string Name { get; set; }
+
+        public int TotalPlayersCount { get; set; }
+
+        public int TeamPlayersCount { get; set; }
+
+        public int GameId { get; set; }
+    }
+}
