09a3606b4127689f631febd6d6550e2d7177e230
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-06-30 22:38:36 +0300

    Updated  TournamentFormat domain model to feature a Description.

--- BESL.Domain/Entities/TournamentFormat.cs
+++ BESL.Domain/Entities/TournamentFormat.cs
@@ -6,6 +6,8 @@
     {
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         public int TotalPlayersCount { get; set; }
 
         public int TeamPlayersCount { get; set; }
