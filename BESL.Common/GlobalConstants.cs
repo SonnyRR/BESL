d84088252b61ef6df0b6597e9dbcc1c058adeb9d
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-03 22:35:40 +0300

    Introduced GlobalConstants helper class in Common layer.

---
+++ BESL.Common/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BESL.Common
+{
+    public class GlobalConstants
+    {
+        #region Exception messages
+
+        public const string ENTITY_ALREADY_DELETED_MSG = "Entity is already deleted.";
+
+        #endregion
+    }
+}
