4920af61f0e4f18ed90eb2e94cc2a103a5ff446d
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-07 19:06:29 +0300

    Implemented TouranmentFormat Modify view and Controller actions.  Did small fixes in views.

--- BESL.Web/Areas/Administration/Controllers/TournamentFormatsController.cs
+++ BESL.Web/Areas/Administration/Controllers/TournamentFormatsController.cs
@@ -9,6 +9,8 @@
     using BESL.Application.TournamentFormats.Queries.GetAll;
     using BESL.Application.TournamentFormats.Commands.Delete;
     using static BESL.Common.GlobalConstants;
+    using BESL.Application.TournamentFormats.Queries.Modify;
+    using BESL.Application.TournamentFormats.Commands.Modify;
 
     public class TournamentFormatsController : AdminController
     {
@@ -24,6 +26,19 @@
             await this.Mediator.Send(command);
 
             _ = this.NotifyService.SendUserSuccessNotificationAsync(command.Name, CREATED_SUCCESSFULLY_MSG, this.UserNameIdentifier);
+            return this.RedirectToAction(nameof(All));
+        }
+
+        public async Task<IActionResult> Modify(ModifyTournamentFormatQuery query)
+        {
+            var viewModel = await this.Mediator.Send(query);
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Modify(ModifyTournamentFormatCommand command)
+        {
+            var viewModel = await this.Mediator.Send(command);
             return this.RedirectToAction(nameof(All));
         }
 
