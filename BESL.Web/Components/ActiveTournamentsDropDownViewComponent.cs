49c0770855357aeb44a5802cb39f83c28c88d9d0
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-09 15:53:31 +0300

    Introduced ActiveTournamentsDropDown view component and view.

---
+++ BESL.Web/Components/ActiveTournamentsDropDownViewComponent.cs
@@ -0,0 +1,27 @@
+﻿namespace BESL.Web.Components
+{
+    using System.Threading.Tasks;
+
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    using BESL.Application.Tournaments.Queries.GetAllTournaments;
+
+    // Does not work without explicitly tagging the class with a ViewComponentAttribute and a display name.
+    [ViewComponent(Name = "ActiveTournamentsDropDownVC")] 
+    public class ActiveTournamentsDropDownViewComponent : ViewComponent
+    {
+        private readonly IMediator mediator;
+
+        public ActiveTournamentsDropDownViewComponent(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var currentActiveTournamentsViewModel = await this.mediator.Send(new GetAllTournamentsQuery());
+            return this.View(currentActiveTournamentsViewModel);
+        }
+    }
+}
