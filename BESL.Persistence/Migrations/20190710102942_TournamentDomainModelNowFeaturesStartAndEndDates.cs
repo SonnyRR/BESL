5df6e15238d2b1d0afe60736bc7938a2e250445f
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-10 13:31:07 +0300

    Added migration.

---
+++ BESL.Persistence/Migrations/20190710102942_TournamentDomainModelNowFeaturesStartAndEndDates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BESL.Persistence.Migrations
+{
+    public partial class TournamentDomainModelNowFeaturesStartAndEndDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Tournaments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Tournaments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Tournaments");
+        }
+    }
+}
