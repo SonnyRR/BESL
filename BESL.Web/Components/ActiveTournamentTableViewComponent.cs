4fa3234b538943cfc1a85a99c923c7f73475f55a
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-10 13:26:15 +0300

    Introduced ActiveTournamentTable view component, updated Games views.

--- BESL.Web/Components/ActiveTournamentTableViewComponent.cs
+++ BESL.Web/Components/ActiveTournamentTableViewComponent.cs
@@ -1,10 +1,23 @@
-﻿using System;
-namespace BESL.Web.Components
+﻿namespace BESL.Web.Components
 {
-    public class ActiveTournamentTableViewComponent
+    using System.Threading.Tasks;
+    using BESL.Application.Tournaments.Queries.GetAllTournaments;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ActiveTournamentsTableViewComponent : ViewComponent
     {
-        public ActiveTournamentTableViewComponent()
+        private readonly IMediator mediator;
+
+        public ActiveTournamentsTableViewComponent(IMediator mediator)
         {
+            this.mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var currentActiveTournamentsViewModel = await this.mediator.Send(new GetAllTournamentsQuery());
+            return this.View(currentActiveTournamentsViewModel);
         }
     }
 }
