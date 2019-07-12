161627db44133dc803d4162b549f51500547332b
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-12 11:34:38 +0300

    Added CheckForVACBans cron job class.

---
+++ BESL.Web/CronJobs/CheckForVACBans.cs
@@ -0,0 +1,14 @@
+﻿namespace BESL.Web.CronJobs
+{
+    using System;
+    using Hangfire;
+
+    public class CheckForVACBans
+    {
+        private const string CRON_SCHEDULE = "";
+
+        public CheckForVACBans()
+        {
+        }
+    }
+}
