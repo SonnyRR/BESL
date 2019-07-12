45ad6a9c612644a818a6c6de209e19381e49a161
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-12 22:33:20 +0300

    Renamed folder and changed namespaces.

---
+++ BESL.Web/Cron/HangfireJobScheduler.cs
@@ -0,0 +1,13 @@
+﻿namespace BESL.Web.Cron
+{
+    using System;
+    using Hangfire;
+     
+    public class HangfireJobScheduler
+    {
+        public static void ScheduleRecurringJobs()
+        {
+            RecurringJob.AddOrUpdate<CheckForVACBans>(x => x.CheckForBans(), CheckForVACBans.CRON_SCHEDULE);
+        }
+    }
+}
