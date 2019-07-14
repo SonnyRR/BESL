71fd43c8478d8fa3acd2a6ba79a0939391e37829
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-14 19:54:03 +0300

    Updated _LoginPartial to be a dropdown menu, introduced PlayerController.

---
+++ BESL.Web/Controllers/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿namespace BESL.Web.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PlayerController : BaseController
+    {
+        public async Task<IActionResult> Details(string id)
+        {
+            return this.View();
+        }
+    }
+}
