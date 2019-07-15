fdfa2c654624972dc0957177aad9a604ce5110cc
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-15 19:51:50 +0300

    Introduced MyTested.AspNetCore.Mvc xUnit testing assembly.

---
+++ BESL.Web.Tests/Components/ActiveTournamentsDropDownTests.cs
@@ -0,0 +1,16 @@
+﻿namespace BESL.Web.Tests.Components
+{
+    using System;
+    using Xunit;
+    using MyTested.AspNetCore.Mvc;
+    using BESL.Web.Components;
+
+    public class ActiveTournamentsDropDownTests
+    {
+        [Fact]
+        public void ComponentShouldReturnNineGenres()
+        {
+           
+        }
+    }
+}
