059299733e91e6f8f41a24d206b7fce75a4f2cf4
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-18 21:30:08 +0300

    Updated UserNotificationHub to send user specific notifications by userId.

--- BESL.Web/Infrastructure/Hubs/UserNotificationHub.cs
+++ BESL.Web/Infrastructure/Hubs/UserNotificationHub.cs
@@ -1,19 +1,33 @@
 ﻿namespace BESL.Web.Infrastructure.Hubs
 {
+    using System.Security.Claims;
     using System.Threading.Tasks;
+    using BESL.Domain.Entities;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.SignalR;
 
     public class UserNotificationHub : Hub
     {
-        public async Task SendUserSuccessNotificationAsync(string messageHeader, string message, string userId)
+        private readonly UserManager<Player> userManager;
+
+        public UserNotificationHub(UserManager<Player> userManager)
         {
+            this.userManager = userManager;
+        }
+
+        public async Task SendUserSuccessNotificationAsync(string messageHeader, string message)
+        {
+            var userId = this.userManager.GetUserId(this.Context.User);
+
             await this.Clients
                 .User(userId)
                 .SendAsync("ReceiveMessageSuccess", messageHeader, message);
         }
 
-        public async Task SendUserFailiureNotificationAsync(string messageHeader, string message, string userId)
+        public async Task SendUserFailiureNotificationAsync(string messageHeader, string message)
         {
+            var userId = this.userManager.GetUserId(this.Context.User);
+
             await this.Clients
                 .User(userId)
                 .SendAsync("ReceiveMessageFailiure", messageHeader, message);
