7049181dd01f12d3318747d9dd04778459136437
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-22 11:08:12 +0300

    Fixed tournament creation bug where the start date could be in the past and the end date was not the minimum of 1 month after the start date.

--- BESL.Application/Tournaments/Commands/Create/CreateTournamentCommandValidator.cs
+++ BESL.Application/Tournaments/Commands/Create/CreateTournamentCommandValidator.cs
@@ -1,5 +1,7 @@
 ﻿namespace BESL.Application.Tournaments.Commands.Create
 {
+    using System;
+
     using FluentValidation;
 
     using BESL.Application.Interfaces;
@@ -9,6 +11,10 @@
     {
         private const int NAME_MIN_LENGTH = 10, NAME_MAX_LENGTH = 50;
         private const int DESC_MIN_LENGTH = 50, DESC_MAX_LENGTH = 500;
+        private const int END_DATE_MIN_MONTH_LENGTH = 1;
+
+        private const string START_DATE_MSG = "Start date cannot be in the past!";
+        private const string END_DATE_MSG = "End date must be at least {0} month after the start date!";
 
         private const string NAME_LENGTH_MSG = "Tournament name length must be between {0} and {1} characters long!";
         private const string DESC_LENGTH_MSG = "Tournament description length must be between {0} and {1} characters long!";
@@ -26,8 +32,12 @@
                 .WithMessage(string.Format(DESC_LENGTH_MSG, DESC_MIN_LENGTH, DESC_MAX_LENGTH));
 
             RuleFor(t => t.StartDate)
-                .GreaterThan(t => new System.DateTime(2015, 1, 1))
-                .WithMessage("edede");
+                .GreaterThanOrEqualTo(t => DateTime.UtcNow.Date)
+                .WithMessage(START_DATE_MSG);
+
+            RuleFor(t => t.EndDate)
+                .GreaterThanOrEqualTo(t => DateTime.UtcNow.Date.AddMonths(1))
+                .WithMessage(string.Format(END_DATE_MSG, END_DATE_MIN_MONTH_LENGTH));
 
             RuleFor(t => t.TournamentImage)
                 .NotEmpty()
