70e5c956a7141b13132e575eece9a3e9a2851713
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-26 12:49:32 +0300

    Introduced TournamentSkillTabless view component, updated ActiveTournamentsTable and PlayerDetailsTeams view component default views, fixed Tournaments/Details view to display correctly large descriptions.

---
+++ BESL.Web/Components/TournamentSkillTablesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿namespace BESL.Web.Components
+{
+    using System;
+    using System.Threading.Tasks;
+    using BESL.Application.TournamentTables.Queries.GetTournamentTables;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TournamentSkillTablesViewComponent : ViewComponent
+    {
+        private readonly IMediator mediator;
+
+        public TournamentSkillTablesViewComponent(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int tournamentId)
+        {
+            var currentActiveTournamentsViewModel = await this.mediator.Send(new GetTournamentTablesQuery() { Id = tournamentId });
+            return this.View(currentActiveTournamentsViewModel);
+        }
+    }
+}
