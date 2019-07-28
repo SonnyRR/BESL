bd2e0a3906ab47ba0d6063fa377811a83f08dc76
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-07-28 23:21:27 +0300

    Added missing CloudinaryFactory.

---
+++ BESL.Infrastructure/Cloudinary/CloudinaryFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace BESL.Infrastructure.Cloudinary
+{
+    using CloudinaryDotNet;
+    using Microsoft.Extensions.Configuration;
+
+    public class CloudinaryFactory
+    {
+        public static Cloudinary GetInstance(IConfiguration configuration)
+        {
+            #warning Don't forget to enter your Cloudinary credentials.
+            /*
+             * dotnet user-secrets set "cloudinary-cloud" "your_cloud_id"
+             * dotnet user-secrets set "cloudinary-apiKey" "your_api_key"
+             * dotnet user-secrets set "cloudinary-apiSecret" "your_api_secret"
+             */
+            var cloud = configuration["cloudinary-cloud"];
+            var apiKey = configuration["cloudinary-apiKey"];
+            var apiSecret = configuration["cloudinary-apiSecret"];
+
+            Account account = new Account(cloud, apiKey, apiSecret);
+
+            Cloudinary instance = new Cloudinary(account);
+            return instance;
+        }
+    }
+}
