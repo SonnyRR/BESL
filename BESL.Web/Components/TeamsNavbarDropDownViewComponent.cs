69d8417cb3ced9b9cebba73d1a2de5689e38d247
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-08-08 20:25:47 +0300

    Introduced TeamsNavbarDropDown view component.

---
+++ BESL.Web/Components/TeamsNavbarDropDownViewComponent.cs
@@ -0,0 +1,28 @@
+﻿namespace BESL.Web.Components
+{
+    using System.Threading.Tasks;
+
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    using BESL.Application.Interfaces;
+    using BESL.Application.Teams.Queries.GetTeamsForPlayer;
+
+    public class TeamsNavbarDropDownViewComponent : ViewComponent
+    {
+        private IMediator mediator;
+        private IUserAcessor userAcessor;
+
+        public TeamsNavbarDropDownViewComponent(IMediator mediator, IUserAcessor userAcessor)
+        {
+            this.mediator = mediator;
+            this.userAcessor = userAcessor;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var viewModel = await this.mediator.Send(new GetTeamsForPlayerQuery { UserId = this.userAcessor.UserId });
+            return this.View(viewModel);
+        }
+    }
+}
