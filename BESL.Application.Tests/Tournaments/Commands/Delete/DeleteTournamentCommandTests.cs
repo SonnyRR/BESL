f5ddf7f8eff5e12c16a9eba5ee93b9c546b3d311
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-08-18 21:50:24 +0300

    Added codecov.yml.

--- BESL.Application.Tests/Tournaments/Commands/Delete/DeleteTournamentCommandTests.cs
+++ BESL.Application.Tests/Tournaments/Commands/Delete/DeleteTournamentCommandTests.cs
@@ -1,15 +1,19 @@
 ﻿namespace BESL.Application.Tests.Tournaments.Commands.Delete
 {
-    using BESL.Application.Tests.Infrastructure;
-    using BESL.Application.Tournaments.Commands.Delete;
-    using BESL.Domain.Entities;
-    using Moq;
-    using Shouldly;
     using System;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+
+    using Moq;
+    using Shouldly;
     using Xunit;
+
+    using BESL.Application.Interfaces;
+    using BESL.Application.Tests.Infrastructure;
+    using BESL.Application.Tournaments.Commands.Delete;
+    using BESL.Domain.Entities;
+    using BESL.Application.Exceptions;
 
     public class DeleteTournamentCommandTests : BaseTest<Tournament>
     {
@@ -30,6 +34,31 @@
 
             var deletedTournament = this.deletableEntityRepository.AllAsNoTrackingWithDeleted().SingleOrDefault(x => x.Id == 2);
             deletedTournament.IsDeleted.ShouldBe(true);
+        }
+
+        [Trait(nameof(Tournament), "DeleteTournament command tests")]
+        [Fact(DisplayName = "Handle given null request should throw ArgumentNullException")]
+
+        public async Task Handle_GivenNullRequest_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var sut = new DeleteTournamentCommandHandler(It.IsAny<IDeletableEntityRepository<Tournament>>());
+
+            // Act & Assert
+            await Should.ThrowAsync<ArgumentNullException>(sut.Handle(null, It.IsAny<CancellationToken>()));
+        }
+
+        [Trait(nameof(Tournament), "DeleteTournament command tests")]
+        [Fact(DisplayName = "Handle given invalid request should throw NotFoundException")]
+
+        public async Task Handle_GivenInvalidRequest_ShouldThrowNotFoundException()
+        {
+            // Arrange
+            var command = new DeleteTournamentCommand { Id = 333 };
+            var sut = new DeleteTournamentCommandHandler(this.deletableEntityRepository);
+
+            // Act & Assert
+            await Should.ThrowAsync<NotFoundException>(sut.Handle(command, It.IsAny<CancellationToken>()));
         }
     }
 }
