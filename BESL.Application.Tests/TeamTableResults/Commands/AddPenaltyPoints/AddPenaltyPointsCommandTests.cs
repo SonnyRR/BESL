3f43c56805955d27ab8397d0c88b39ea684f0ddc
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2019-08-19 23:12:57 +0300

    Added TeamTableResults command tests.

---
+++ BESL.Application.Tests/TeamTableResults/Commands/AddPenaltyPoints/AddPenaltyPointsCommandTests.cs
@@ -0,0 +1,61 @@
+﻿namespace BESL.Application.Tests.TeamTableResults.Commands.AddPenaltyPoints
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Moq;
+    using Shouldly;
+    using Xunit;
+
+    using BESL.Application.Exceptions;
+    using BESL.Application.Interfaces;
+    using BESL.Application.TeamTableResults.Commands.AddPenaltyPoints;
+    using BESL.Application.Tests.Infrastructure;
+    using BESL.Domain.Entities;
+
+    public class AddPenaltyPointsCommandTests : BaseTest<TeamTableResult>
+    {
+        [Trait(nameof(TeamTableResult), "AddPenaltyPoints command tests")]
+        [Fact(DisplayName = "Handle given valid request add penalty points")]
+        public async Task Handle_GivenValidRequest_ShouldDropTeam()
+        {
+            // Arrange
+            var command = new AddPenaltyPointsCommand { TeamTableResultId = 1, PenaltyPoints = 5 };
+            var sut = new AddPenaltyPointsCommandHandler(this.deletableEntityRepository);
+
+            // Act
+            var affectedRows = await sut.Handle(command, It.IsAny<CancellationToken>());
+
+            // Assert
+            affectedRows.ShouldBe(1);
+
+            var updatedEntity = this.deletableEntityRepository.AllAsNoTracking().SingleOrDefault(x => x.Id == 1);
+            updatedEntity.PenaltyPoints.ShouldBe(5);
+        }
+
+        [Trait(nameof(TeamTableResult), "AddPenaltyPoints command tests")]
+        [Fact(DisplayName = "Handle given null request add penalty points")]
+        public async Task Handle_GivenNullRequest_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var sut = new AddPenaltyPointsCommandHandler(It.IsAny<IDeletableEntityRepository<TeamTableResult>>());
+
+            // Act & Assert
+            await Should.ThrowAsync<ArgumentNullException>(sut.Handle(null, It.IsAny<CancellationToken>()));
+        }
+
+        [Trait(nameof(TeamTableResult), "AddPenaltyPoints command tests")]
+        [Fact(DisplayName = "Handle given invalid request add penalty points")]
+        public async Task Handle_GivenInvalidRequest_ShouldThrowNotFoundException()
+        {
+            // Arrange
+            var command = new AddPenaltyPointsCommand { TeamTableResultId = 1124124, PenaltyPoints = It.IsAny<int>() };
+            var sut = new AddPenaltyPointsCommandHandler(this.deletableEntityRepository);
+
+            // Act & Assert
+            await Should.ThrowAsync<NotFoundException>(sut.Handle(command, It.IsAny<CancellationToken>()));
+        }
+    }
+}
---
+++ BESL.Application.Tests/TeamTableResults/Commands/Drop/DropTeamTableResultCommandTests.cs
@@ -0,0 +1,61 @@
+﻿namespace BESL.Application.Tests.TeamTableResults.Commands.Drop
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Moq;
+    using Shouldly;
+    using Xunit;
+
+    using BESL.Application.Exceptions;
+    using BESL.Application.Interfaces;
+    using BESL.Application.TeamTableResults.Commands.Drop;
+    using BESL.Application.Tests.Infrastructure;
+    using BESL.Domain.Entities;
+    using System.Linq;
+
+    public class DropTeamTableResultCommandTests : BaseTest<TeamTableResult>
+    {
+        [Trait(nameof(TeamTableResult), "DropTeamTableResult command tests")]
+        [Fact(DisplayName = "Handle given valid request should drop team")]
+        public async Task Handle_GivenValidRequest_ShouldDropTeam()
+        {
+            // Arrange
+            var command = new DropTeamTableResultCommand { TeamTableResultId = 1 };
+            var sut = new DropTeamTableResultCommandHandler(this.deletableEntityRepository);
+
+            // Act
+            var affectedRows = await sut.Handle(command, It.IsAny<CancellationToken>());
+
+            // Assert
+            affectedRows.ShouldBe(1);
+
+            var updatedEntity = this.deletableEntityRepository.AllAsNoTracking().SingleOrDefault(x => x.Id == 1);
+            updatedEntity.IsDropped.ShouldBe(true);
+        }
+
+        [Trait(nameof(TeamTableResult), "DropTeamTableResult command tests")]
+        [Fact(DisplayName = "Handle given null request throw ArgumentNullException")]
+        public async Task Handle_GivenNullRequest_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var sut = new DropTeamTableResultCommandHandler(It.IsAny<IDeletableEntityRepository<TeamTableResult>>());
+
+            // Act & Assert
+            await Should.ThrowAsync<ArgumentNullException>(sut.Handle(null, It.IsAny<CancellationToken>()));
+        }
+
+        [Trait(nameof(TeamTableResult), "DropTeamTableResult command tests")]
+        [Fact(DisplayName = "Handle given invalid request should throw NotFoundException")]
+        public async Task Handle_GivenInvalidRequest_ThrowNotFoundException()
+        {
+            // Arrange
+            var command = new DropTeamTableResultCommand { TeamTableResultId = 1134 };
+            var sut = new DropTeamTableResultCommandHandler(this.deletableEntityRepository);
+
+            // Act & Assert
+            await Should.ThrowAsync<NotFoundException>(sut.Handle(command, It.IsAny<CancellationToken>()));
+        }
+    }
+}
