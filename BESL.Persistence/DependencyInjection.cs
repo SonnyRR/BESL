0cf20a0f508928cbefb32882f7e7a129e102456f
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2020-10-03 22:46:38 +0300

    Added dependency injection extension methods for each application layer.

---
+++ BESL.Persistence/DependencyInjection.cs
@@ -0,0 +1,54 @@
+﻿namespace BESL.Persistence
+{
+    using System;
+    
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using BESL.Application.Interfaces;
+    using BESL.Entities;
+    using BESL.SharedKernel;
+    using BESL.Persistence.Repositories;
+    using BESL.Persistence.Infrastructure;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString(DbConnectionStringHandler.GetConnectionStringNameForCurrentOS())));
+
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString(DbConnectionStringHandler.GetConnectionStringNameForCurrentOS())));
+
+            services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.Configure<RedisConfigurationOptions>(configuration.GetSection("Redis"));
+            services.AddTransient(typeof(IRedisService<>), typeof(RedisService<>));
+
+            services.AddTransient<IUserStore<Player>, ApplicationUserStore>();
+            services.AddTransient<IRoleStore<PlayerRole>, ApplicationRoleStore>();
+
+            services.AddIdentity<Player, PlayerRole>(
+                opt =>
+                {
+                    opt.Password.RequiredLength = 6;
+                    opt.Password.RequireDigit = true;
+                    opt.Password.RequireLowercase = true;
+                    opt.Password.RequireUppercase = true;
+                    opt.Password.RequireNonAlphanumeric = false;
+                })
+                .AddUserStore<ApplicationUserStore>()
+                .AddRoleStore<ApplicationRoleStore>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddDefaultUI();
+
+            return services;
+        }
+    }
+}
--- BESL.Web/Startup.cs
+++ BESL.Web/Startup.cs
@@ -1,37 +1,19 @@
 ﻿namespace BESL.Web
 {
-    using System.Linq;
-    using System.Reflection;
-
-    using AutoMapper;
     using FluentValidation.AspNetCore;
     using Hangfire;
-    using MediatR;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Identity.UI.Services;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-
+    
     using BESL.Application;
-    using BESL.Application.Infrastructure;
     using BESL.Application.Interfaces;
-    using BESL.Application.Infrastructure.AutoMapper;
-    using BESL.Application.Infrastructure.Validators;
-    using BESL.SharedKernel;
-    using BESL.Entities;
     using BESL.Infrastructure;
-    using BESL.Infrastructure.Messaging;
-    using BESL.Infrastructure.Cloudinary;
     using BESL.Infrastructure.Hubs;
-    using BESL.Infrastructure.CronJobs;
     using BESL.Persistence;
-    using BESL.Persistence.Seeding;
-    using BESL.Persistence.Repositories;   
-    using BESL.Persistence.Infrastructure;
+    using BESL.SharedKernel;
     using BESL.Web.Filters;
     using BESL.Web.Infrastructure;
     using BESL.Web.Middlewares;
@@ -45,6 +27,17 @@
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationInsightsTelemetry();
+            services.AddInfrastructureLayer(this.Configuration);
+            services.AddPersistenceLayer(this.Configuration);
+            services.AddApplicationLayer(this.Configuration);
+            services.AddSignalR();
+            services.AddHttpContextAccessor();
+            services.AddAuthentication().AddSteam();
+            services.AddRazorPages();
+            services.AddHangfire(cfg => cfg.UseSqlServerStorage(this.Configuration.GetConnectionString(DbConnectionStringHandler.GetHangfireConnectionStringNameForCurrentOS())));
+            services.AddControllersWithViews()
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<IApplicationDbContext>())
+                .AddRazorRuntimeCompilation();
 
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -52,89 +45,10 @@
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(
-                        this.Configuration
-                            .GetConnectionString(DbConnectionStringHandler.GetConnectionStringNameForCurrentOS())));
-
-            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
-                    options.UseSqlServer(
-                        this.Configuration
-                            .GetConnectionString(DbConnectionStringHandler.GetConnectionStringNameForCurrentOS())));
-
-            services.AddTransient<IUserStore<Player>, ApplicationUserStore>();
-            services.AddTransient<IRoleStore<PlayerRole>, ApplicationRoleStore>();
-
-            services.AddIdentity<Player, PlayerRole>(
-                opt =>
-                {
-                    opt.Password.RequiredLength = 6;
-                    opt.Password.RequireDigit = true;
-                    opt.Password.RequireLowercase = true;
-                    opt.Password.RequireUppercase = true;
-                    opt.Password.RequireNonAlphanumeric = false;
-                })
-                .AddUserStore<ApplicationUserStore>()
-                .AddRoleStore<ApplicationRoleStore>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders()
-                .AddDefaultUI();
-
-            services.AddAuthentication().AddSteam();
-
-            services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).Assembly });
-
-            services.AddRazorPages();
-            services.AddControllersWithViews()
-                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<ApplicationDependencyInjectionHelper>())
-                .AddRazorRuntimeCompilation();
-
-            services.AddMediatR(typeof(ApplicationDependencyInjectionHelper).Assembly);
-
-            services.AddTransient<IDateTime, MachineDateTime>();
-
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CustomExceptionNotificationBehaviour<,>));
-            
-            services.AddScoped<IFileValidate, GameImageFileValidate>();
-            services.AddScoped<INotifyService, UserNotificationHub>();
-
-            services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
-            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-
-            services.AddSignalR();
-            services.AddHttpContextAccessor();
-
-            services.AddHangfire(cfg => cfg.UseSqlServerStorage(
-                this.Configuration.GetConnectionString(DbConnectionStringHandler.GetHangfireConnectionStringNameForCurrentOS())));
-
-            services.AddTransient<CheckForVACBans>();
-
-            services.AddTransient<IEmailSender, BeslMessageSender>();
-            services.AddTransient<ISmsSender, BeslMessageSender>();
-
-            services.AddScoped<IUserAccessor, UserAccessor>();
-
-            services.AddSingleton(x => CloudinaryFactory.GetInstance(this.Configuration));
-            services.AddTransient<ICloudinaryHelper, CloudinaryHelper>();
-
-            services.Configure<RedisConfigurationOptions>(Configuration.GetSection("Redis"));
-            services.AddTransient(typeof(IRedisService<>), typeof(RedisService<>));
         }
     
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            using (var serviceScope = app.ApplicationServices.CreateScope())
-            {
-                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                if (env.EnvironmentName == GlobalConstants.ENV_PRODUCTION)
-                {
-                   this.ApplyMigrations(dbContext);
-                }
-
-                new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
-            }
-
             if (env.EnvironmentName == "Development")
             {
                 app.UseDeveloperExceptionPage();
@@ -149,10 +63,8 @@
             app.UseRouting();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseHangfireServer();
             app.UseHangfireDashboard("/Administration/hangfire", new DashboardOptions()
             {
@@ -160,11 +72,8 @@
                 Authorization = new[] { new DashboardAuthorizationFilter() }
             });
 
-            HangfireJobScheduler.ScheduleRecurringJobs();
-
             app.UseNotificationHandlerMiddleware();
             app.UseCustomExceptionHandlerMiddleware();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapHub<UserNotificationHub>("/userNotificationHub");
@@ -178,14 +87,8 @@
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
-        }
 
-        private void ApplyMigrations(ApplicationDbContext context)
-        {
-            if (context.Database.GetPendingMigrations().Any())
-            {
-                context.Database.Migrate();
-            }
+            HangfireJobScheduler.ScheduleRecurringJobs();
         }
     }
 }
