30944b7b15d46f4976d805b50e4fbad4ff64eac0
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2020-10-03 22:46:38 +0300

    Added dependency injection extension methods for each application layer.

---
+++ BESL.Application/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿namespace BESL.Application
+{
+    using System;
+    using System.Reflection;
+
+    using AutoMapper;
+    using MediatR;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using BESL.Application.Infrastructure;
+    using BESL.Application.Infrastructure.AutoMapper;
+    using BESL.Application.Infrastructure.Validators;
+    using BESL.Application.Interfaces;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            services.AddMediatR(typeof(DependencyInjection).Assembly);
+            services.AddAutoMapper(new Assembly[] { typeof(AutoMapperProfile).Assembly });
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CustomExceptionNotificationBehaviour<,>));
+            services.AddScoped<IFileValidate, GameImageFileValidate>();
+
+            return services;
+        }
+    }
+}
---
+++ BESL.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿namespace BESL.Infrastructure
+{
+    using System;
+
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using BESL.Application.Interfaces;
+    using BESL.Infrastructure.Cloudinary;
+    using BESL.Infrastructure.Hubs;
+    using BESL.Infrastructure.Messaging;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            services.AddTransient<IDateTime, MachineDateTime>();
+            services.AddScoped<INotifyService, UserNotificationHub>();
+            services.AddTransient<IEmailSender, BeslMessageSender>();
+            services.AddTransient<ISmsSender, BeslMessageSender>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddSingleton(x => CloudinaryFactory.GetInstance(configuration));
+            services.AddTransient<ICloudinaryHelper, CloudinaryHelper>();
+
+            return services;
+        }
+    }
+}
