aeb695ebbdec3ba3e7b424ac68e4b694415dbd17
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2021-06-06 12:36:39 +0300

    Added NUKE build targets for building & pushing Docker images to the GitHub image registry.

--- build/Build.cs
+++ build/Build.cs
@@ -5,24 +5,46 @@ using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
+using Nuke.Common.Tools.Docker;
+using static Nuke.Common.Tools.Docker.DockerTasks;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.GitHub.GitHubTasks;
+using Nuke.Common.CI.GitHubActions;
 
 [CheckBuildProjectConfigurations(TimeoutInMilliseconds = 1_500)]
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
+    public Build()
+    {
+        // Redirect output from STERR to STDOUT.
+        DockerLogger = (_, message) => Logger.Normal(message);
+    }
+
     public static int Main() => Execute<Build>(x => x.Compile);
+
+    readonly string GitHubImageRegistry = "docker.pkg.github.com";
+    readonly string ImageName = "besl.web";
+    static bool IsHostGitHubAction => Host is GitHubActions;
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter(
+        "The PAT used in order to push the Docker image to the container registry as an owner of the repository")]
+    readonly string GitHubPersonalAccessToken;
+
+    [Parameter("The GitHub user account that will be used to push the Docker image to the container registry")]
+    readonly string GitHubUsername;
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
@@ -80,4 +102,50 @@ class Build : NukeBuild
                             .SetCoverletOutput($"{projectRoutePrefix}_coverage.xml");
                     })), Environment.ProcessorCount, true);
         });
+
+    Target BuildDockerImage => _ => _
+        .Executes(() =>
+        {
+            DockerBuild(s => s
+                .SetProcessEnvironmentVariable("DOCKER_BUILDKIT", "1")
+                .SetProcessWorkingDirectory(RootDirectory)
+                .SetFile(RootDirectory / "Dockerfile")
+                .SetPath(".")
+                .SetTag(ImageName));
+        });
+
+    Target PublishDockerImage => _ => _
+        .DependsOn(BuildDockerImage)
+        // .OnlyWhenDynamic(() => GitRepository.IsOnMasterBranch())
+        .Requires(
+            () => GitHubUsername,
+            () => GitHubPersonalAccessToken)
+        .Executes(() =>
+        {
+            var (repositoryOwner, repositoryName) = GetGitHubRepositoryInfo(GitRepository);
+
+            DockerLogin(s => s
+                .SetServer(GitHubImageRegistry)
+                .SetUsername(GitHubUsername)
+                .SetPassword(GitHubPersonalAccessToken)
+                .EnableProcessLogOutput());
+
+            PushDockerImageWithTag("latest", repositoryOwner, repositoryName);
+        });
+
+    void PushDockerImageWithTag(string tag, string repositoryOwner, string repositoryName)
+    {
+        tag = tag?.ToLower();
+        repositoryOwner = repositoryOwner?.ToLower();
+        repositoryName = repositoryName?.ToLower();
+
+        var targetImageName = $"{GitHubImageRegistry}/{repositoryOwner}/{repositoryName}/{ImageName}:{tag}";
+
+        DockerTag(s => s
+            .SetSourceImage(ImageName)
+            .SetTargetImage(targetImageName));
+
+        Logger.Normal($"Pushing Docker image with tag: {tag}");
+        DockerPush(s => s.SetName(targetImageName));
+    }
 }
